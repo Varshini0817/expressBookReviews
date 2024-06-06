3beee410acd7c6efc4be879b98148ed217901841
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-06-06 19:42:09 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,14 +7,27 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if(username && password)
+  {
+    if(!isValid(username)){
+        users.push({"username":username, "password":password});
+        return res.status(200).json({message:"User successfully regeistered!"})
+    }
+    else{
+        return res.status(404).json({message: "User already exists!"});
+    }
+}
+    return res.status(404).json({message: "Unable to get details"})
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
   res.send(JSON.stringify(books,null,4));
-  return res.status(300).json({message: "Yet to be implemented"});
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
@@ -88,3 +101,4 @@ public_users.get('/review/:isbn',function (req, res) {
 });
 
 module.exports.general = public_users;
+
